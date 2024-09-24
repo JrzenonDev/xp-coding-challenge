f52c41077608c8f74a97d0901f6ce01e9867474c
JrzenonDev <jrobertoonb@gmail.com>
2024-09-24 14:30:15 -0300

    (feat)[feature/main] create bullets with infos chart

--- src/components/DashBoard/index.tsx
+++ src/components/DashBoard/index.tsx
@@ -5,32 +5,23 @@ import React from "react";
 import { PieChart, Pie, Cell } from "recharts";
 
 const data = [
-  { name: "Group A", value: 900 },
-  { name: "Group B", value: 100 },
-  { name: "Group C", value: 100 },
-  { name: "Group D", value: 100 },
+  { name: "DUNNO", value: 900, color: "#855CF8" },
+  { name: "I’m Boring", value: 150, color: "#E289F2" },
+  { name: "Money", value: 150, color: "#3f2780" },
+  { name: "Fun", value: 150, color: "#a98cfa" },
 ];
-
-const COLORS = ["#855CF8", "#ACB9FF", "#3f2780", "#E289F2"];
 
 export function DashBoard() {
   return (
-    <Card
-      style={{
-        width: 300,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-        }}
-      >
+    <Card className="w-[300px] flex justify-center items-center">
+      <div className="h-full flex justify-center items-center flex-col">
+        <div>
+          <h2 className="text-[18px] text-gray-700">
+            Why do you create a startup?
+          </h2>
+          <p className="text-[15px] text-gray-500">Trends</p>
+        </div>
+
         <PieChart width={200} height={200}>
           <Pie
             data={data}
@@ -45,13 +36,22 @@ export function DashBoard() {
             stroke="none"
           >
             {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
+              <Cell key={`cell-${index}`} fill={entry.color} />
             ))}
           </Pie>
         </PieChart>
+
+        <div className="flex w-full gap-7">
+          {data.map((item, index) => (
+            <div key={index} className="flex flex-col items-start">
+              <div
+                className="w-[13.5px] h-[13.5px] rounded-[50%]"
+                style={{ backgroundColor: item.color }}
+              />
+              <p className="text-sm">{item.name}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </Card>
   );
