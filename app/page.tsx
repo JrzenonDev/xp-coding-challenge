69c8b3ebce6b3086859be0299ccba78361aa6b7f
JrzenonDev <jrobertoonb@gmail.com>
2024-09-24 00:50:16 -0300

    (feat)[feature/main] styles in page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { DashBoard } from "@/src/components/DashBoard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
       <DashBoard />
     </main>
   );
